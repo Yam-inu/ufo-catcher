4418ebb7d663abb5480a6c97ad7280977ed5c373
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-01-30 09:11:56 +0900

    Update mjpg_streamer_init.sh
    不要な場所の削除

--- mjpg_streamer_init.sh
+++ mjpg_streamer_init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#コピペ元
+#参照
 #https://nekonogorogoro.com/raspberrypi_car_vol3/
-
-#以下のコマンドを実行後に実行できる
-#sed -i 's/\r//' *.sh
 
 sudo apt update
 sudo apt install build-essential imagemagick libv4l-dev libjpeg-dev cmake -y
@@ -12,5 +9,3 @@ git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer/mjpg-streamer-experimental
 sudo make
 sudo make install
-
-sudo ./mjpg_streamer -i "./input_uvc.so -f 10 -r 640x480 -d /dev/video0 -y -n" -o "./output_http.so -w ./www -p 8080" 
