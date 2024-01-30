ba297e2d1d471078310dae1a2ebd99405b65c3f8
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-01-30 09:14:37 +0900

    Update start_server.sh
    コメントの追加

--- start_server.sh
+++ start_server.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-PORT="8080"
-SIZE="320x240"
-FRAMERATE="30"
+#設定
+PORT="8080"          #ポート番号
+SIZE="320x240"       #解像度
+FRAMERATE="30"       #フレームレート
 
+#webiopiスタート
 echo "start webiopi"
 sudo systemctl start webiopi
 
+#mjpg-streamerスタート
 echo "start mjpg-streamer"
 cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
 sudo ./mjpg_streamer -i "./input_uvc.so -f $FRAMERATE -r $SIZE -d /dev/video0 -y -n" -o "./output_http.so -w ./www -p $PORT"
-
