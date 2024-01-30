ece6cd403d15b43311b6fccb60c1fa29fe6aee83
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-01-30 09:19:34 +0900

    Update webiopi_init.sh
    コメントの追加

--- webiopi_init.sh
+++ webiopi_init.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-#コピペ元
+#参照
 #https://nekonogorogoro.com/raspberrypi_car_vol2/
 
-
-#以下のコマンドを実行後に実行できる
-#sed -i 's/\r//' *.sh
-
+#メインファイルのインストール
 wget https://sourceforge.net/projects/webiopi/files/WebIOPi-0.7.1.tar.gz
 tar xvzf WebIOPi-0.7.1.tar.gz
 
 cd WebIOPi-0.7.1
 
+#修正パッチのインストール、パッチ適用
 wget https://raw.githubusercontent.com/doublebind/raspi/master/webiopi-pi2bplus.patch
 patch -p1 -i webiopi-pi2bplus.patch
 
+# ./setupのうちバージョン変更等でインストールできなくなったものをインストール
 sudo apt update
 sudo apt install python-dev-is-python3 -y
 sudo apt install python-setuptools-doc -y
+
+#セットアップ
 sudo ./setup.sh
 
-
+#systemctlでwebiopiを使えるようにする
 cd /etc/systemd/system/
 sudo wget https://raw.githubusercontent.com/doublebind/raspi/master/webiopi.service
-
-sudo systemctl start webiopi
