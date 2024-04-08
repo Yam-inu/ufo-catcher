204d0f8dadd2496ce8ed656bbe2350d6061149eb
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-04-08 09:13:48 +0900

    Update start_server.sh
    誤り修正

--- start_server.sh
+++ start_server.sh
@@ -11,5 +11,5 @@ sudo systemctl start webiopi
 
 #mjpg-streamerスタート
 echo "start mjpg-streamer"
-cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+cd /home/pi/ufo-catcher/mjpg-streamer/mjpg-streamer-experimental
 sudo ./mjpg_streamer -i "./input_uvc.so -f $FRAMERATE -r $SIZE -d /dev/video0 -y -n" -o "./output_http.so -w ./www -p $PORT"
