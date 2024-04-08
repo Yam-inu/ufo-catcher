629ca6b3b93f5a8dfe81ca1a0cc7ad3a954ffc6d
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-04-08 09:21:08 +0900

    Update other_init.sh
    機能追加

--- other_init.sh
+++ other_init.sh
@@ -12,3 +12,10 @@ echo "static domain_name_servers=192.168.11.1" >> dhcpcd.conf
 
 # start_server.shの移動
 sudo mv start_server.sh /usr/local/bin/start_server
+
+#webIOPiで参照されるファイルの入れ替え
+cd /home/pi/ufo-catcher/webIOPi-0.7.1/htdocs
+mkdir original_file
+mv *.* original_file
+cd /home/pi/ufo-catcher
+mv index.html raspi_car_sp.py motor.js ./webIOPi-0.7.1/htdocs/
